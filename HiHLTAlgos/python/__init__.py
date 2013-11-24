b4aa593d282f4665baa02596e994fe1f0780c9ac
Ta-Wei <ta-wei.wang@cern.ch>
2013-11-24 06:21:22 +0100

    Add HLT

---
+++ HiHLTAlgos/python/__init__.py
@@ -0,0 +1,3 @@
+#Automatically created by SCRAM
+import os
+__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/CmsHi/HiHLTAlgos/',1)[0])+'/cfipython/slc5_amd64_gcc462/CmsHi/HiHLTAlgos')
