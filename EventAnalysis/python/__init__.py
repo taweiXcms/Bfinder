8b94edb2d911207fd5dcd9d54aeab4c87a7c254f
Ta Wei <ta-wei.wang@cern.ch>
2015-09-01 11:12:20 -0400

    remove DataFormats HeavyIonsAnalysis EventAnalysis RecoHI

--- EventAnalysis/python/__init__.py
+++
@@ -1,3 +0,0 @@
-#Automatically created by SCRAM
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/Bfinder/EventAnalysis/',1)[0])+'/cfipython/slc6_amd64_gcc472/Bfinder/EventAnalysis')
