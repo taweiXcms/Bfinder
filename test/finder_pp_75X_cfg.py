7a627cc20ab2d3f8f35b14c1e344c8a854399881
Jian Sun <sun229@purdue.edu>
2016-12-22 22:22:00 -0500

    update Dpt cut

--- test/finder_pp_75X_cfg.py
+++ test/finder_pp_75X_cfg.py
@@ -257,7 +257,7 @@ if ppDHFdefault and not ppDMBdefault and not ppBdefault and not ppD0DstarV2:
 ##pp Dfinder setting on MB
 if ppDMBdefault and not ppDHFdefault and not ppBdefault and not ppD0DstarV2:
     process.Dfinder.tkPtCut = cms.double(0.7)#before fit
-    process.Dfinder.dPtCut = cms.vdouble(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0)#before fit
+    process.Dfinder.dPtCut = cms.vdouble(0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0)#before fit
     process.Dfinder.dCutSeparating_PtVal = cms.vdouble(5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5.)
     process.Dfinder.tktkRes_svpvDistanceCut_lowptD = cms.vdouble(0., 0., 0., 0., 0., 0., 0., 0., 2.5, 2.5, 2.5, 2.5)
     process.Dfinder.tktkRes_svpvDistanceCut_highptD = cms.vdouble(0., 0., 0., 0., 0., 0., 0., 0., 2.5, 2.5, 2.5, 2.5)
