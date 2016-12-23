5ea64918f2a5d495e84da9bb981a9e468f1e1e0c
Jian Sun <sun229@purdue.edu>
2016-12-22 22:16:08 -0500

    update pp cfg

--- test/finder_pp_75X_cfg.py
+++ test/finder_pp_75X_cfg.py
@@ -256,14 +256,14 @@ if ppDHFdefault and not ppDMBdefault and not ppBdefault and not ppD0DstarV2:
     process.p = cms.Path(process.DfinderSequence)
 ##pp Dfinder setting on MB
 if ppDMBdefault and not ppDHFdefault and not ppBdefault and not ppD0DstarV2:
-    process.Dfinder.tkPtCut = cms.double(1.)#before fit
+    process.Dfinder.tkPtCut = cms.double(0.7)#before fit
     process.Dfinder.dPtCut = cms.vdouble(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0)#before fit
     process.Dfinder.dCutSeparating_PtVal = cms.vdouble(5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5.)
     process.Dfinder.tktkRes_svpvDistanceCut_lowptD = cms.vdouble(0., 0., 0., 0., 0., 0., 0., 0., 2.5, 2.5, 2.5, 2.5)
     process.Dfinder.tktkRes_svpvDistanceCut_highptD = cms.vdouble(0., 0., 0., 0., 0., 0., 0., 0., 2.5, 2.5, 2.5, 2.5)
     process.Dfinder.svpvDistanceCut_lowptD = cms.vdouble(4.0, 4.0, 2.5, 2.5, 2.5, 2.5, 4.0, 4.0, 0., 0., 0., 0.)
     process.Dfinder.svpvDistanceCut_highptD = cms.vdouble(2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 0., 0., 0., 0.)
-    process.Dfinder.Dchannel = cms.vint32(1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0)
+    process.Dfinder.Dchannel = cms.vint32(1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
     process.p = cms.Path(process.DfinderSequence)
 ## default cut version 2
 if ppD0DstarV2 and not ppDHFdefault and not ppDMBdefault and not ppBdefault:
