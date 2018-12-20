7157640dfa3e70f7fbba28642c75c2369d4aa7ff
Jing <boundino@gmail.com>
2018-12-20 22:08:59 +0100

    add Bfinder to forest script

---
+++ test/DnBfinder_to_Forest_103X.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+PATHTOTEST=$CMSSW_BASE/src/HeavyIonsAnalysis/JetAnalysis/test
+
+##
+cp ${PATHTOTEST}/runForestAOD_pponAA_DATA_103X.py ${PATHTOTEST}/runForestAOD_pponAA_DATA_103X_wDfinder.py
+
+echo '
+#################### D/B finder ################# 
+AddCaloMuon = False 
+runOnMC = False 
+HIFormat = False 
+UseGenPlusSim = False 
+VtxLabel = "offlinePrimaryVerticesWithBS" 
+TrkLabel = "generalTracks" 
+from Bfinder.finderMaker.finderMaker_75X_cff import finderMaker_75X 
+finderMaker_75X(process, AddCaloMuon, runOnMC, HIFormat, UseGenPlusSim, VtxLabel, TrkLabel)
+process.Dfinder.MVAMapLabel = cms.InputTag(TrkLabel,"MVAValues")
+process.Dfinder.makeDntuple = cms.bool(True)
+process.Dfinder.tkPtCut = cms.double(1.) # before fit
+process.Dfinder.dPtCut = cms.vdouble(2.0, 2.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0) # before fit
+process.Dfinder.VtxChiProbCut = cms.vdouble(0.05, 0.05, 0.0, 0.0, 0.0, 0.0, 0.05, 0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.05, 0.05)
+process.Dfinder.dCutSeparating_PtVal = cms.vdouble(5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5., 5.)
+process.Dfinder.tktkRes_svpvDistanceCut_lowptD = cms.vdouble(0., 0., 0., 0., 0., 0., 0., 0., 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 0., 0.)
+process.Dfinder.tktkRes_svpvDistanceCut_highptD = cms.vdouble(0., 0., 0., 0., 0., 0., 0., 0., 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 0., 0.)
+process.Dfinder.svpvDistanceCut_lowptD = cms.vdouble(2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 0., 0., 0., 0., 0., 0., 2.5, 2.5)
+process.Dfinder.svpvDistanceCut_highptD = cms.vdouble(2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 0., 0., 0., 0., 0., 0., 2.5, 2.5)
+process.Dfinder.Dchannel = cms.vint32(1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1)
+
+process.dfinder = cms.Path(process.DfinderSequence)
+
+' >> ${PATHTOTEST}/runForestAOD_pponAA_DATA_103X_wDfinder.py
+
+cp ${PATHTOTEST}/runForestAOD_pponAA_DATA_103X_wDfinder.py ${PATHTOTEST}/runForestAOD_pponAA_DATA_103X_onlyDfinder.py
+
+echo '
+process.ana_step = cms.Path(
+    process.HiForest +
+    process.hltanalysis +
+    process.hltobject +
+    process.centralityBin +
+    process.hiEvtAnalyzer 
+    )
+' >> ${PATHTOTEST}/runForestAOD_pponAA_DATA_103X_onlyDfinder.py
+
+##
+cp ${PATHTOTEST}/runForestAOD_pponAA_DATA_103X.py ${PATHTOTEST}/runForestAOD_pponAA_DATA_103X_wBfinder.py
+
+echo '
+#################### D/B finder #################
+AddCaloMuon = False
+runOnMC = False
+HIFormat = False
+UseGenPlusSim = False
+VtxLabel = "offlinePrimaryVerticesWithBS"
+TrkLabel = "generalTracks"
+from Bfinder.finderMaker.finderMaker_75X_cff import finderMaker_75X
+finderMaker_75X(process, AddCaloMuon, runOnMC, HIFormat, UseGenPlusSim, VtxLabel, TrkLabel)
+
+process.Bfinder.MVAMapLabel = cms.InputTag(TrkLabel,"MVAValues")
+process.Bfinder.makeBntuple = cms.bool(True)
+process.Bfinder.tkPtCut = cms.double(1.0)#before fit
+process.Bfinder.jpsiPtCut = cms.double(0.0)#before fit
+process.Bfinder.bPtCut = cms.vdouble(5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0)#before fit
+process.Bfinder.Bchannel = cms.vint32(1, 0, 0, 1, 1, 1, 1)
+process.Bfinder.VtxChiProbCut = cms.vdouble(0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05)
+process.Bfinder.svpvDistanceCut = cms.vdouble(2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0)
+process.Bfinder.doTkPreCut = cms.bool(True)
+process.Bfinder.MuonTriggerMatchingPath = cms.vstring(
+    "HLT_HIL1DoubleMu0_v1",
+    "HLT_HIL1DoubleMu0_part1_v1",
+    "HLT_HIL1DoubleMu0_part2_v1",
+    "HLT_HIL1DoubleMu0_part3_v1",
+    "HLT_HIL1DoubleMu0_2HF_v1",
+    "HLT_HIL1DoubleMu0_2HF0_v1",
+    "HLT_HIL1DoubleMu10_v1",
+    "HLT_HIL2DoubleMu0_NHitQ_v2",
+    "HLT_HIL2DoubleMu0_NHitQ_2HF_v1",
+    "HLT_HIL2DoubleMu0_NHitQ_2HF0_v1",
+    "HLT_HIL1DoubleMu0_2HF_Cent30100_v1",
+    "HLT_HIL1DoubleMu0_2HF0_Cent30100_v1",
+    "HLT_HIL2DoubleMu0_2HF_Cent30100_NHitQ_v1",
+    "HLT_HIL1DoubleMu0_Cent30_v1",
+    "HLT_HIL2DoubleMu0_2HF0_Cent30100_NHitQ_v1",
+    "HLT_HIL2DoubleMu0_Cent30_NHitQ_v1",
+    "HLT_HIL2DoubleMu0_Cent30_OS_NHitQ_v1",
+    "HLT_HIL3DoubleMu0_Cent30_v1",
+    "HLT_HIL3DoubleMu0_Cent30_OS_m2p5to4p5_v1",
+    "HLT_HIL3DoubleMu0_Cent30_OS_m7to14_v1",
+    "HLT_HIL3DoubleMu0_OS_m2p5to4p5_v1",
+    "HLT_HIL3DoubleMu0_OS_m7to14_v1")
+process.Bfinder.MuonTriggerMatchingFilter = cms.vstring("hltHIDoubleMu0L1Filtered",
+                                                        "hltHIDoubleMu0MinBiasL1Filtered",
+                                                        "hltHIDoubleMu0HFTower0Filtered",
+                                                        "hltHIDoubleMu10L1Filtered",
+                                                        "hltHIL2DoubleMu0NHitQFiltered",
+                                                        "hltHIL2DoubleMu0NHitQ2HFFiltered",
+                                                        "hltHIL2DoubleMu0NHitQ2HF0Filtered",
+                                                        "hltHIDoubleMu0MinBiasCent30to100L1Filtered",
+                                                        "hltHIDoubleMu0HFTower0Cent30to100L1Filtered",
+                                                        "hltHIL2DoubleMu02HFcent30100NHitQFiltered",
+                                                        "hltHIDoubleMu0MinBiasCent30L1Filtered",
+                                                        "hltHIL2DoubleMu02HF0cent30100NHitQFiltered",
+                                                        "hltHIL2DoubleMu0cent30NHitQFiltered",
+                                                        "hltHIL2DoubleMu0cent30OSNHitQFiltered",
+                                                        "hltHIDimuonOpenCentrality30L3Filter",
+                                                        "hltHIDimuonOpenCentrality30OSm2p5to4p5L3Filter",
+                                                        "hltHIDimuonOpenCentrality30OSm7to14L3Filter",
+                                                        "hltHIDimuonOpenOSm2p5to4p5L3Filter",
+                                                        "hltHIDimuonOpenOSm7to14L3Filter")
+process.p = cms.Path(process.BfinderSequence)
+
+' >> ${PATHTOTEST}/runForestAOD_pponAA_DATA_103X_wBfinder.py
+
+cp ${PATHTOTEST}/runForestAOD_pponAA_DATA_103X_wBfinder.py ${PATHTOTEST}/runForestAOD_pponAA_DATA_103X_onlyBfinder.py
+
+echo '
+process.ana_step = cms.Path(
+    process.HiForest +
+    process.hltanalysis +
+    process.hltobject +
+    process.centralityBin +
+    process.hiEvtAnalyzer 
+    )
+' >> ${PATHTOTEST}/runForestAOD_pponAA_DATA_103X_onlyBfinder.py
+
+##
